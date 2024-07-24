0f0d01e510b433e3faa5b2ddec18f1ec1c4b02e4
TwentySixB26 <srijawamusaitomo@gmail.com>
2024-07-24 17:29:00 +0700

    again

--- public/scricpt.js
+++ public/scricpt.js
@@ -1,6 +1,20 @@
+window.onscroll = function () {
+    const header = document.querySelector('header')
+    const fixedNav = header.offsetTop
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed')
+    }else{
+        header.classList.remove('navbar-fixed')
+    }
+}
+
+
+
 const menuToogle = document.querySelector('#menuToogle')
 const navbarMenu = document.querySelector('header div div ul')
 const closeNav = document.querySelector('.closeNav') 
+
 
 menuToogle.addEventListener('click', function() {
     navbarMenu.classList.toggle('muncul')
