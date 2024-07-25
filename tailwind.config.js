a24df68a6e394948a563967ce44e7e30ca553b83
TwentySixB26 <srijawamusaitomo@gmail.com>
2024-07-25 23:50:11 +0700

    part 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,9 @@ module.exports = {
         '2xl' : '1320px'
       } ,
       boxShadow : {
-        'shadow-me' : '5px 7px 25px rgba(0, 0, 0, 0.6)'
-
-      }
+        'shadow-me' : '5px 7px 25px rgba(0, 0, 0, 0.6)',
+        'shadow-me2' : '2px 4px 10px rgba(0, 0, 0, 0.4)'
+      } 
     },
   },
   plugins: [],
